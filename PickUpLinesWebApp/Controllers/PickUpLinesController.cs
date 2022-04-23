6627bf7b0e06dae54a4fb3a0d69a885650136ba7
Brittany Ridley <britt.ridley@outlook.com>
2022-04-23 14:53:54 -0500

    Add More

--- PickUpLinesWebApp/Controllers/PickUpLinesController.cs
+++ PickUpLinesWebApp/Controllers/PickUpLinesController.cs
@@ -25,6 +25,12 @@ namespace PickUpLinesWebApp.Controllers
             return View(await _context.PickUpLine.ToListAsync());
         }
 
+        // GET: PickUpLines/Search for pick up lines
+        public async Task<IActionResult> ShowSearchForm()
+        {
+            return View();
+        }
+
         // GET: PickUpLines/Details/5
         public async Task<IActionResult> Details(int? id)
         {
